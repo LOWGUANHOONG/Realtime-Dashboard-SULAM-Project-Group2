3c2cb76c0f44ab850102816ee280741ec42c3cee
LOW GUAN HOONG <lowguanhoong2@gmail.com>
2026-01-11 08:18:10 +0800

    move socketio

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,15 @@ from flask_cors import CORS
 
 socketio = SocketIO()
 
+socketio.init_app(app, async_mode='gevent', cors_allowed_origins="*", logger=True, engineio_logger=True)
+
 def create_app():
     # static_folder tells Flask where index.html and script.js are
     # static_url_path='' allows index.html to find style.css at root level
     app = Flask(__name__, static_folder='static', static_url_path='')
 
     CORS(app)
-    socketio.init_app(app, async_mode='gevent', cors_allowed_origins="*", logger=True, engineio_logger=True)
+    
 
     # Register the blueprint without a forced prefix so '/' works for the UI
     from .api.routes import api_bp
